182c403c7f6ba7cda681bc605a5027e60053136e
Minmin Tian <mtian@mtian-mbp15.guidewire.com>
2018-07-26 16:31:29 -0400

    installed the bootstrap

--- la-search-app/src/App.js
+++ la-search-app/src/App.js
@@ -10,6 +10,19 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Live Analytics Search</h1>
         </header>
+        <SearchField/>
+      </div>
+    );
+  }
+}
+
+class SearchField extends Component {
+  render() {
+    return (
+      <div>
+        <div className="search-section">
+          This is the search field.
+        </div>
       </div>
     );
   }
