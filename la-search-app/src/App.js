60b69a0413c1967fa3a826cb8931a087a2fdf269
Minmin Tian <mtian@mtian-mbp15.guidewire.com>
2018-07-30 14:29:11 -0400

    installed the fonts.

--- la-search-app/src/App.js
+++ la-search-app/src/App.js
@@ -21,10 +21,13 @@ class SearchField extends Component {
     return (
       <div className="search-wrapper">
         <div className="search-section">
-          <form>
-            <span className="gw-logo-blue"></span>
+          <form className="search-form">
             <div>
-              <input type="text" className="form-control border-top-0 border-left-0 border-right-0"></input>
+              <span className="gw-logo-blue"></span>
+            </div>
+            <div className="md-form">
+              <input className="form-control border-top-0 border-left-0 border-right-0" type="text" placeholder="What would you like to explore in your data?" aria-label="Search"/>
+              <span className="icon-search"></span>
             </div>
           </form>
         </div>
