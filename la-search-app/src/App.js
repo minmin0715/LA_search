903055d028a276a7c307cc030b00d48415b8d5eb
Minmin Tian <mtian@mtian-mbp15.guidewire.com>
2018-07-30 15:42:01 -0400

    Migrated the icons to images folder

--- la-search-app/src/App.js
+++ la-search-app/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
-import logo from './gw_logo_white.svg';
+import logo from './assets/images/gw_logo_white.svg';
 import './App.scss';
-
+import SearchField from './js/SearchField';
 class App extends Component {
   render() {
     return (
@@ -16,24 +16,6 @@ class App extends Component {
   }
 }
 
-class SearchField extends Component {
-  render() {
-    return (
-      <div className="search-wrapper">
-        <div className="search-section">
-          <form className="search-form">
-            <div>
-              <span className="gw-logo-blue"></span>
-            </div>
-            <div className="md-form">
-              <input className="form-control border-top-0 border-left-0 border-right-0" type="text" placeholder="What would you like to explore in your data?" aria-label="Search"/>
-              <span className="icon-search"></span>
-            </div>
-          </form>
-        </div>
-      </div>
-    );
-  }
-}
+
 
 export default App;
---
+++ la-search-app/src/js/SearchField.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+class SearchField extends Component {
+    render() {
+      return (
+        <div className="search-wrapper">
+          <div className="search-section">
+            <form className="search-form">
+              <div>
+                <span className="gw-logo-blue"></span>
+              </div>
+              <div className="md-form">
+                <input className="form-control border-top-0 border-left-0 border-right-0" type="text" placeholder="What would you like to explore in your data?" aria-label="Search"/>
+                <span className="icon-search"></span>
+              </div>
+            </form>
+          </div>
+        </div>
+      );
+    }
+  }
+
+  export default SearchField;
