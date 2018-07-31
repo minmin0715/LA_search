64febb96da2cd32dfe77ae6e9a6601df06051334
Minmin Tian <mtian@mtian-mbp15.guidewire.com>
2018-07-31 15:16:19 -0400

    Added the custom fonts.

--- la-search-app/src/js/SearchCard.js
+++ la-search-app/src/js/SearchCard.js
@@ -2,10 +2,10 @@ import React, { Component } from 'react';
 
 class SearchCard extends Component {
     render() {
-    const cardMessage = "What are the claim processing activities by week?"
+    
         return (
             <div className="search-card-section">
-                <p className="card-content">{cardMessage}</p>
+                <p className="card-content">What are the <span className="keywords">claim</span> processing <span className="keywords">activities</span> by <span className="keywords">week?</span></p>
                 <div className="times-number">
                     89
                 </div>
