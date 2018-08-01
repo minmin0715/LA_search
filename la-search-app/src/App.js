cc732e3afe29abe2686bc55895dd8a4db70fdb5c
Minmin Tian <mtian@mtian-mbp15.guidewire.com>
2018-08-01 13:28:59 -0400

    Fixed some issues.

--- la-search-app/src/App.js
+++ la-search-app/src/App.js
@@ -26,13 +26,13 @@ class App extends Component {
             </div>
           </div>
           <div className="search-card">
-            <SearchCard/>
-            <SearchCard/>
-            <SearchCard/>
-            <SearchCard/>
+            <SearchCard question="What are the <span class='keywords'>claim</span> processing <span class='keywords'>activities</span> by <span class='keywords'>week</span>?" number="89"/>
+            <SearchCard question="What is the <span class='keywords'>trend</span> for how long it takes <span class='keywords'>claims to close</span> by <span class='keywords'>month</span>?" number="67"/>
+            <SearchCard question="How <span class='keywords'>profitable</span> was my <span class='keywords'>auto LOB year over year</span>?" number="16"/>
+            <SearchCard question="What are the <span class='keywords'>claim</span> processing <span class='keywords'>activities</span> by <span class='keywords'>week</span>?" number="10"/>
           </div>
         </div>
-        
+       
       </div>
     );
   }
--- la-search-app/src/js/SearchCard.js
+++ la-search-app/src/js/SearchCard.js
@@ -5,10 +5,8 @@ class SearchCard extends Component {
     
         return (
             <div className="search-card-section">
-                <p className="card-content">What are the <span className="keywords">claim</span> processing <span className="keywords">activities</span> by <span className="keywords">week?</span></p>
-                <div className="times-number">
-                    89
-                </div>
+                <p className="card-content" dangerouslySetInnerHTML={{ __html: this.props.question }}></p>
+                <div className="times-number">{this.props.number}</div>
             </div>
         );
     }
