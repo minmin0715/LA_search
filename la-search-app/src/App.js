d012c33488a9eef4d66716437ae2ce1d7cc3b3ba
Minmin Tian <mtian@mtian-mbp15.guidewire.com>
2018-08-01 17:03:11 -0400

    Refactored the tab component.

--- la-search-app/src/App.js
+++ la-search-app/src/App.js
@@ -6,6 +6,26 @@ import ResultsTabs from './js/ResultsTabs';
 import SearchCard from './js/SearchCard';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showMoreFrequentlyQuestions: true,
+      showMoreMyQuestions: true
+    };
+    this.toggleFrequentlyQuestions = this.toggleFrequentlyQuestions.bind(this);
+    this.toggleMyQuestions = this.toggleMyQuestions.bind(this);
+  }
+  toggleFrequentlyQuestions = () => {
+    this.setState(prevState => ({
+      showMoreFrequentlyQuestions: !prevState.showMoreFrequentlyQuestions
+    }));
+  }
+
+  toggleMyQuestions = () => {
+    this.setState(prevState => ({
+      showMoreMyQuestions: !prevState.showMoreMyQuestions
+    }));
+  }
   render() {
     return (
       <div className="App">
@@ -14,25 +34,27 @@ class App extends Component {
           <h1 className="App-title">Live Analytics Search</h1>
         </header>
         <div className="search-content">
-          <SearchField/>
-          <div className="tabs-group">
-            <div className="most-questions">
-              <ResultsTabs title="Most frequently asked questions"/>
-              <hr/>
-            </div>
-            <div className="my-questions">
-              <ResultsTabs title="My questions"/>
-              <hr/>
-            </div>
-          </div>
+          <SearchField />
+              <ResultsTabs
+                toggleFrequentlyQuestions={this.toggleFrequentlyQuestions}
+                showMoreFrequentlyQuestions={this.state.showMoreFrequentlyQuestions}
+                toggleMyQuestions={this.toggleMyQuestions}
+                showMoreMyQuestions={this.state.showMoreMyQuestions}/>
           <div className="search-card">
-            <SearchCard question="What are the <span class='keywords'>claim</span> processing <span class='keywords'>activities</span> by <span class='keywords'>week</span>?" number="89"/>
-            <SearchCard question="What is the <span class='keywords'>trend</span> for how long it takes <span class='keywords'>claims to close</span> by <span class='keywords'>month</span>?" number="67"/>
-            <SearchCard question="How <span class='keywords'>profitable</span> was my <span class='keywords'>auto LOB year over year</span>?" number="16"/>
-            <SearchCard question="What are the <span class='keywords'>claim</span> processing <span class='keywords'>activities</span> by <span class='keywords'>week</span>?" number="10"/>
+            <SearchCard 
+              question="What are the <span class='keywords'>claim</span> processing <span class='keywords'>activities</span> by <span class='keywords'>week</span>?" 
+              number="89"/>
+            <SearchCard 
+              question="What is the <span class='keywords'>trend</span> for how long it takes <span class='keywords'>claims to close</span> by <span class='keywords'>month</span>?" 
+              number="67"/>
+            <SearchCard 
+              question="How <span class='keywords'>profitable</span> was my <span class='keywords'>auto LOB year over year</span>?" 
+              number="16" />
+            <SearchCard 
+              question="What are the <span class='keywords'>claim</span> processing <span class='keywords'>activities</span> by <span class='keywords'>week</span>?" 
+              number="10" />
           </div>
         </div>
-       
       </div>
     );
   }
--- la-search-app/src/js/ResultsTabs.js
+++ la-search-app/src/js/ResultsTabs.js
@@ -1,14 +1,35 @@
 import React, { Component } from 'react';
 
 class ResultsTabs extends Component {
-    render() {
-      return (
-        <div className="tab-section">
-            <span className="text">{this.props.title}</span>
-            <span className="icon-arrow"></span>
+  
+  render() {
+    return (
+      <div className="tabs-container">
+        <div className="tab-section" onClick={this.props.toggleFrequentlyQuestions}>
+            <span className="text">Most frequently asked questions</span>
+            {
+              this.props.showMoreFrequentlyQuestions &&
+              <span className="icon-arrow"></span>
+            }
+            {
+              !this.props.showMoreFrequentlyQuestions &&
+              <span>Click me</span>
+            }
         </div>
-      );
-    }
+        <div className="tab-section" onClick={this.props.toggleMyQuestions}>
+          <span className="text">My questions</span>
+          {
+            this.props.showMoreMyQuestions &&
+            <span className="icon-arrow"></span>
+          }
+          {
+            !this.props.showMoreMyQuestions &&
+            <span>Click me</span>
+          }
+      </div>
+    </div>
+    );
   }
+}
 
   export default ResultsTabs;
