e0bb4a779d019cc64f9f5f40b4350543e2ba4a3b
Minmin Tian <mtian@mtian-mbp15.guidewire.com>
2018-08-02 14:47:14 -0400

    Fixed the blue border for tab.

--- la-search-app/src/js/ResultsTabs.js
+++ la-search-app/src/js/ResultsTabs.js
@@ -4,30 +4,33 @@ class ResultsTabs extends Component {
   
   render() {
     return (
-      <div className="tabs-container">
-        <div className="tab-section" onClick={this.props.toggleFrequentlyQuestions}>
-            <span className="text">Most frequently asked questions</span>
+      <div className="tabs-group">
+        <div className="tabs-container">
+          <div className="tab-section" onClick={this.props.toggleFrequentlyQuestions}>
+              <span className="text">Most frequently asked questions</span>
+              {
+                this.props.showMoreFrequentlyQuestions &&
+                <span className="icon-arrow"></span>
+              }
+              {
+                !this.props.showMoreFrequentlyQuestions &&
+                <span className="icon-arrow"></span>
+              }
+          </div>
+          <div className="tab-section" onClick={this.props.toggleMyQuestions}>
+            <span className="text">My questions</span>
             {
-              this.props.showMoreFrequentlyQuestions &&
+              this.props.showMoreMyQuestions &&
               <span className="icon-arrow"></span>
             }
             {
-              !this.props.showMoreFrequentlyQuestions &&
-              <span>Click me</span>
+              !this.props.showMoreMyQuestions &&
+              <span className="icon-arrow"></span>
             }
+          </div>
         </div>
-        <div className="tab-section" onClick={this.props.toggleMyQuestions}>
-          <span className="text">My questions</span>
-          {
-            this.props.showMoreMyQuestions &&
-            <span className="icon-arrow"></span>
-          }
-          {
-            !this.props.showMoreMyQuestions &&
-            <span>Click me</span>
-          }
+        <hr/>
       </div>
-    </div>
     );
   }
 }
