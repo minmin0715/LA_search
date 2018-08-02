649542c4c1eb712d92721ff3048ce88288e3003e
Minmin Tian <mtian@mtian-mbp15.guidewire.com>
2018-08-02 11:49:49 -0400

    Worked on the alignment of the search card.

--- la-search-app/src/js/SearchCard.js
+++ la-search-app/src/js/SearchCard.js
@@ -5,28 +5,71 @@ class SearchCard extends Component {
         var questions = [
             {
                 name: "What are the <span class='keywords'>claim</span> processing <span class='keywords'>activities</span> by <span class='keywords'>week</span>?" ,
-                number: 89
+                number: 89,
+                index: 1
             },
             {
                 name: "What is the <span class='keywords'>trend</span> for how long it takes <span class='keywords'>claims to close</span> by <span class='keywords'>month</span>?",
-                number: 67
+                number: 67,
+                index: 2
             },
             {
                 name: "How <span class='keywords'>profitable</span> was my <span class='keywords'>auto LOB year over year</span>?",
-                number: 16
+                number: 16,
+                index: 3
             },
             {
                 name: "What are the <span class='keywords'>claim</span> processing <span class='keywords'>activities</span> by <span class='keywords'>week</span>?",
-                number: 10
+                number: 10,
+                index: 4
+            },   {
+                name: "What are the <span class='keywords'>claim</span> processing <span class='keywords'>activities</span> by <span class='keywords'>week</span>?" ,
+                number: 89,
+                index: 5
+            },
+            {
+                name: "What is the <span class='keywords'>trend</span> for how long it takes <span class='keywords'>claims to close</span> by <span class='keywords'>month</span>?",
+                number: 67,
+                index: 6
+            },
+            {
+                name: "How <span class='keywords'>profitable</span> was my <span class='keywords'>auto LOB year over year</span>?",
+                number: 16,
+                index: 7
+            },
+            {
+                name: "What are the <span class='keywords'>claim</span> processing <span class='keywords'>activities</span> by <span class='keywords'>week</span>?",
+                number: 10,
+                index: 8
+            },
+            {
+                name: "What are the <span class='keywords'>claim</span> processing <span class='keywords'>activities</span> by <span class='keywords'>week</span>?" ,
+                number: 89,
+                index: 9
+            },
+            {
+                name: "What is the <span class='keywords'>trend</span> for how long it takes <span class='keywords'>claims to close</span> by <span class='keywords'>month</span>?",
+                number: 67,
+                index: 10
+            },
+            {
+                name: "How <span class='keywords'>profitable</span> was my <span class='keywords'>auto LOB year over year</span>?",
+                number: 16,
+                index: 11
+            },
+            {
+                name: "What are the <span class='keywords'>claim</span> processing <span class='keywords'>activities</span> by <span class='keywords'>week</span>?",
+                number: 10,
+                index: 12
             }
         ]
         return (
             <div className="search-card">
                 {questions.map((question) => 
-                    <div className="search-card-section">
-                    <p className="card-content" dangerouslySetInnerHTML={{ __html: question.name }}></p>
-                    <div className="times-number">{question.number}</div>
-                </div>
+                    <div key={question.index} className="search-card-section">
+                        <p className="card-content" dangerouslySetInnerHTML={{ __html: question.name}}></p>
+                        <div className="times-number">{question.number}</div>
+                    </div>
                 )}   
             </div>
         );
