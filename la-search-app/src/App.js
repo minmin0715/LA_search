0ea312b89e3037ca73024634cc4e505cb05bbe50
Minmin Tian <mtian@mtian-mbp15.guidewire.com>
2018-08-02 10:55:03 -0400

    Refactored the search card component.

--- la-search-app/src/App.js
+++ la-search-app/src/App.js
@@ -27,6 +27,7 @@ class App extends Component {
     }));
   }
   render() {
+
     return (
       <div className="App">
         <header className="App-header">
@@ -40,20 +41,8 @@ class App extends Component {
                 showMoreFrequentlyQuestions={this.state.showMoreFrequentlyQuestions}
                 toggleMyQuestions={this.toggleMyQuestions}
                 showMoreMyQuestions={this.state.showMoreMyQuestions}/>
-          <div className="search-card">
-            <SearchCard 
-              question="What are the <span class='keywords'>claim</span> processing <span class='keywords'>activities</span> by <span class='keywords'>week</span>?" 
-              number="89"/>
-            <SearchCard 
-              question="What is the <span class='keywords'>trend</span> for how long it takes <span class='keywords'>claims to close</span> by <span class='keywords'>month</span>?" 
-              number="67"/>
-            <SearchCard 
-              question="How <span class='keywords'>profitable</span> was my <span class='keywords'>auto LOB year over year</span>?" 
-              number="16" />
-            <SearchCard 
-              question="What are the <span class='keywords'>claim</span> processing <span class='keywords'>activities</span> by <span class='keywords'>week</span>?" 
-              number="10" />
-          </div>
+         
+          <SearchCard/>
         </div>
       </div>
     );
--- la-search-app/src/js/SearchCard.js
+++ la-search-app/src/js/SearchCard.js
@@ -2,11 +2,32 @@ import React, { Component } from 'react';
 
 class SearchCard extends Component {
     render() {
-    
+        var questions = [
+            {
+                name: "What are the <span class='keywords'>claim</span> processing <span class='keywords'>activities</span> by <span class='keywords'>week</span>?" ,
+                number: 89
+            },
+            {
+                name: "What is the <span class='keywords'>trend</span> for how long it takes <span class='keywords'>claims to close</span> by <span class='keywords'>month</span>?",
+                number: 67
+            },
+            {
+                name: "How <span class='keywords'>profitable</span> was my <span class='keywords'>auto LOB year over year</span>?",
+                number: 16
+            },
+            {
+                name: "What are the <span class='keywords'>claim</span> processing <span class='keywords'>activities</span> by <span class='keywords'>week</span>?",
+                number: 10
+            }
+        ]
         return (
-            <div className="search-card-section">
-                <p className="card-content" dangerouslySetInnerHTML={{ __html: this.props.question }}></p>
-                <div className="times-number">{this.props.number}</div>
+            <div className="search-card">
+                {questions.map((question) => 
+                    <div className="search-card-section">
+                    <p className="card-content" dangerouslySetInnerHTML={{ __html: question.name }}></p>
+                    <div className="times-number">{question.number}</div>
+                </div>
+                )}   
             </div>
         );
     }
