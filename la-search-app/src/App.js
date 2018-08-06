e2eaaedc645a92978b9ace4a19b0dbcb2744f2ed
Minmin Tian <mtian@mtian-mbp15.guidewire.com>
2018-08-06 12:34:36 -0400

    Refactored the search tab component.

--- la-search-app/src/App.js
+++ la-search-app/src/App.js
@@ -6,48 +6,21 @@ import ResultsTabs from './js/ResultsTabs';
 import SearchCard from './js/SearchCard';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      showMoreFrequentlyQuestions: true,
-      showMoreMyQuestions: true
-    };
-    this.toggleFrequentlyQuestions = this.toggleFrequentlyQuestions.bind(this);
-    this.toggleMyQuestions = this.toggleMyQuestions.bind(this);
-  }
-  toggleFrequentlyQuestions = () => {
-    this.setState(prevState => ({
-      showMoreFrequentlyQuestions: !prevState.showMoreFrequentlyQuestions
-    }));
-  }
-
-  toggleMyQuestions = () => {
-    this.setState(prevState => ({
-      showMoreMyQuestions: !prevState.showMoreMyQuestions
-    }));
-  }
-  render() {
-
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Live Analytics Search</h1>
-        </header>
-        <div className="search-content">
-          <SearchField />
-              <ResultsTabs
-                toggleFrequentlyQuestions={this.toggleFrequentlyQuestions}
-                showMoreFrequentlyQuestions={this.state.showMoreFrequentlyQuestions}
-                toggleMyQuestions={this.toggleMyQuestions}
-                showMoreMyQuestions={this.state.showMoreMyQuestions}/>
-              <SearchCard/>
-        </div>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="App">
+				<header className="App-header">
+					<img src={logo} className="App-logo" alt="logo" />
+					<h1 className="App-title">Live Analytics Search</h1>
+				</header>
+				<div className="search-content">
+					<SearchField />
+					<ResultsTabs/>
+					<SearchCard/>
+				</div>
+			</div>
+		);
+	}
 }
-
-
 
 export default App;
--- la-search-app/src/js/ResultsTabs.js
+++ la-search-app/src/js/ResultsTabs.js
@@ -1,38 +1,57 @@
 import React, { Component } from 'react';
 
 class ResultsTabs extends Component {
-  
-  render() {
-    return (
-      <div className="tabs-group">
-        <div className="tabs-container">
-          <div className="tab-section" onClick={this.props.toggleFrequentlyQuestions}>
-              <span className="text">Most frequently asked questions</span>
-              {
-                this.props.showMoreFrequentlyQuestions &&
-                <span className="icon-arrow-down"></span>
-              }
-              {
-                !this.props.showMoreFrequentlyQuestions &&
-                <span className="icon-arrow-up"></span>
-              }
-          </div>
-          <div className="tab-section" onClick={this.props.toggleMyQuestions}>
-            <span className="text">My questions</span>
-            {
-              this.props.showMoreMyQuestions &&
-              <span className="icon-arrow-down"></span>
-            }
-            {
-              !this.props.showMoreMyQuestions &&
-              <span className="icon-arrow-up"></span>
-            }
-          </div>
-        </div>
-        <hr/>
-      </div>
-    );
-  }
+	constructor(props) {
+		super(props);
+		this.state = {
+			showMoreFrequentlyQuestions: true,
+			showMoreMyQuestions: true
+		};
+		this.toggleFrequentlyQuestions = this.toggleFrequentlyQuestions.bind(this);
+		this.toggleMyQuestions = this.toggleMyQuestions.bind(this);
+	}
+	toggleFrequentlyQuestions = () => {
+		this.setState(prevState => ({
+			showMoreFrequentlyQuestions: !prevState.showMoreFrequentlyQuestions
+		}));
+	}
+
+	toggleMyQuestions = () => {
+		this.setState(prevState => ({
+			showMoreMyQuestions: !prevState.showMoreMyQuestions
+		}));
+	}
+	render() {
+		return (
+			<div className="tabs-group">
+				<div className="tabs-container">
+					<div className="tab-section" onClick={this.toggleFrequentlyQuestions}>
+						<span className="text">Most frequently asked questions</span>
+						{
+							this.state.showMoreFrequentlyQuestions &&
+							<span className="icon-arrow-down"></span>
+						}
+						{
+							!this.state.showMoreFrequentlyQuestions &&
+							<span className="icon-arrow-up"></span>
+						}
+					</div>
+					<div className="tab-section" onClick={this.toggleMyQuestions}>
+						<span className="text">My questions</span>
+						{
+						this.state.showMoreMyQuestions &&
+						<span className="icon-arrow-down"></span>
+						}
+						{
+						!this.state.showMoreMyQuestions &&
+						<span className="icon-arrow-up"></span>
+						}
+					</div>
+				</div>
+				<hr/>
+			</div>
+		);
+	}
 }
 
-  export default ResultsTabs;
+export default ResultsTabs;
