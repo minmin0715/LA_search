5e59761080cf770322f4caac5c01f1287aa54b92
Minmin Tian <mtian@mtian-mbp15.guidewire.com>
2018-08-06 09:55:08 -0400

    Worked on the toggle arrows.

--- la-search-app/src/js/ResultsTabs.js
+++ la-search-app/src/js/ResultsTabs.js
@@ -10,22 +10,22 @@ class ResultsTabs extends Component {
               <span className="text">Most frequently asked questions</span>
               {
                 this.props.showMoreFrequentlyQuestions &&
-                <span className="icon-arrow"></span>
+                <span className="icon-arrow-down"></span>
               }
               {
                 !this.props.showMoreFrequentlyQuestions &&
-                <span className="icon-arrow"></span>
+                <span className="icon-arrow-up"></span>
               }
           </div>
           <div className="tab-section" onClick={this.props.toggleMyQuestions}>
             <span className="text">My questions</span>
             {
               this.props.showMoreMyQuestions &&
-              <span className="icon-arrow"></span>
+              <span className="icon-arrow-down"></span>
             }
             {
               !this.props.showMoreMyQuestions &&
-              <span className="icon-arrow"></span>
+              <span className="icon-arrow-up"></span>
             }
           </div>
         </div>
